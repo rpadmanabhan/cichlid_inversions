b9bf0272b21e86e4d083e70bc502fa1c15c92b0f
raghav <raghus606@gmail.com>
2015-11-30 11:09:24 -0500

    Made small changes

--- run_blast.py
+++ run_blast.py
@@ -26,8 +26,24 @@ with open(inversion_file,'r') as INV_FILE:
         print (scaffold)
         inversion = mzebra_ref[scaffold][int(start):int(stop)].seq
         inversion_seq = return_bioseq_obj(inversion)
-        with open('temp.fasta','w') as TEMP:
-            TEMP.write(">%s_%s_%s\n"%(scaffold,start,stop))
-            TEMP.write(inversion)
-            out_file = scaffold+"_"+start+"_"+stop 
+        if(start==0):
+            inversion_bp_1 = mzebra_ref[scaffold][int(start):int(start)+1000].seq
+        elif(stop == len(inversion)):
+            inversion_bp_2 = mzebra_ref[scaffold][int(stop)-1000:int(stop)].seq
+        else:
+            inversion_bp_1 = mzebra_ref[scaffold][int(start):int(start)+1000].seq
+            inversion_bp_2 = mzebra_ref[scaffold][int(stop)-1000:int(stop)].seq
+        with open('temp.fasta','w') as TEMP, open('scaffold.fasta','w') as SCAFFOLD:
+            #TEMP.write(">%s_%s_%s\n"%(scaffold,start,stop))
+            #TEMP.write(inversion)
+            TEMP.write(">bp1\n")
+            TEMP.write(inversion_bp_1+"\n")
+            TEMP.write(">bp2\n")
+            TEMP.write(inversion_bp_2)
+            out_file = scaffold+"_"+start+"_"+stop+".xml"
+            SCAFFOLD.write(">%s\n"%(scaffold))
+            SCAFFOLD.write("%s"%(mzebra_ref[scaffold][0:].seq))
+            subprocess.check_output("makeblastdb -in scaffold.fasta -input_type fasta -dbtype nucl -title m_zebra -out temp_mz_v0_db",shell=True)
             subprocess.check_output("blastn -db %s -query temp.fasta -out %s -outfmt 5 -num_threads 2 -evalue 1e-20"%(sys.argv[3],out_file),shell=True)
+            #subprocess.check_output("rm temp_mz*",shell=True)
+            subprocess.check_output("rm scaffold.fasta",shell=True)
